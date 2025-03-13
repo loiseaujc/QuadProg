45c0283ea3ae17371ca66c39e981391c21afd4b3
Jean-Christophe <loiseau.jc@gmail.com>
2025-03-13 19:40:18 +0100

    Add interface for qpgen2

--- src/QuadProg.f90
+++ src/QuadProg.f90
@@ -2,7 +2,13 @@ module QuadProg
   implicit none
   private
 
-  public :: qpgen1
   public :: qpgen2
+
+  interface
+    subroutine qpgen2(dmat, dvec, fddmat, n, sol, lagr, crval, amat, bvec, fdamat, q, meq, iact, nact, iter, work, ierr)
+      integer :: fddmat, n, fdamat, q, meq, iact(*), nact, iter(*), ierr
+      double precision :: dmat(fddmat, *), dvec(*), lagr(*), sol(*), bvec(*), work(*), amat(fdamat, *), crval
+    end subroutine
+  end interface
 
 end module QuadProg
