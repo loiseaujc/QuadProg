bee2434a922043d246e18bd05faf6e8dc17654c8
Jean-Christophe <loiseau.jc@gmail.com>
2025-03-13 19:40:33 +0100

    Prep' for unit tests.

--- test/check.f90
+++ test/check.f90
@@ -1,6 +1,35 @@
 program check
+    ! Fortran Standard.
+    use, intrinsic :: iso_fortran_env, only: error_unit, output_unit
+    ! Unit-test utilities.
+    use testdrive, only: run_testsuite, new_testsuite, testsuite_type
+    ! Dense solver for Convex Quadratic Programs.
+    use QuadProg
+    ! Collection of test problems.
     use TestProblems
     implicit none
 
-    print *, "Put some tests in here!"
+    ! Unit-test related.
+    integer :: status, i
+    type(testsuite_type), allocatable :: testsuites(:)
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+    ! Collection of test suites.
+    status = 0
+    testsuites = [new_testsuite("Simple problems", collect_test_problems)]
+
+    ! Run all the test suites.
+    do i = 1, size(testsuites)
+        write (output_unit, *) "-----"
+        write (output_unit, fmt) "Testing :", testsuites(i)%name
+        write (output_unit, *) "-----", new_line('a')
+        call run_testsuite(testsuites(i)%collect, error_unit, status)
+        write (output_unit, *) new_line('a')
+    end do
+
+    if (status > 0) then
+        write (error_unit, '(i0, 1x, a)') status, "test(s) failed!"
+    else if (status == 0) then
+        write (output_unit, *) "All tests succesfully passed!", new_line('a')
+    end if
 end program check
--- test/problems.f90
+++ test/problems.f90
@@ -1,6 +1,16 @@
 module TestProblems
+    use testdrive, only: new_unittest, unittest_type, error_type, check
     implicit none
     private
-contains
 
+    public :: collect_test_problems
+
+contains
+    subroutine collect_test_problems(testsuite)
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
+    end subroutine
+
+    subroutine test_problem_1(error)
+        type(error_type), allocatable, intent(out) :: error
+    end subroutine
 end module
