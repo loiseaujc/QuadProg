fb0c8e0faaa7cef09d0d4e199406b6b6ec082af6
Jean-Christophe <loiseau.jc@gmail.com>
2025-04-10 15:44:30 +0200

    Added legacy version of problem n°3.

--- test/problems.f90
+++ test/problems.f90
@@ -22,8 +22,9 @@ contains
                   new_unittest("Unconstrained QP problem", test_problem_1), &
                   new_unittest("(Legacy) Unconstrained QP problem", test_legacy_problem_1), &
                   new_unittest("Inequality Constrained QP problem", test_problem_2), &
-                  new_unittest("(Legacy) Inequality Constrained QP problem", test_problem_2), &
-                  new_unittest("Equality Constrained QP problem", test_problem_3) &
+                  new_unittest("(Legacy) Inequality Constrained QP problem", test_legacy_problem_2), &
+                  new_unittest("Equality Constrained QP problem", test_problem_3), &
+                  new_unittest("(Legacy) Equality Constrained QP problem", test_legacy_problem_3) &
                   ]
    end subroutine
 
@@ -216,6 +217,68 @@ contains
    end subroutine
 
    ! Test case from https://github.com/quadprog/quadprog/issues/2#issue-443570242
+   subroutine test_legacy_problem_3(error)
+      type(error_type), allocatable, intent(out) :: error
+      integer, parameter :: n = 5, m = 3
+      ! Size of the problem.
+      real(dp) :: P(n, n), q(n), obj
+      real(dp), allocatable :: A(:, :), b(:)
+      real(dp), allocatable :: x(:), y(:)
+      real(dp), allocatable :: work(:)
+      integer               :: i, neq, ncons, r, lwork, nact, iter(2), info
+      integer, allocatable  :: iact(:)
+      real(dp) :: xref(n), yref(m), obj_ref
+      ! Reference solution, Lagrange multipliers, reference cost.
+
+      ! Setup problem.
+      P = 0.0_dp
+      do concurrent(i=1:n)
+         P(i, i) = 1.0_dp
+      end do
+      q = [0.73727161_dp, 0.75526241_dp, 0.04741426_dp, -0.11260887_dp, -0.11260887_dp]
+      allocate (A(n, m))
+      A(1, :) = [3.6_dp, 0.0_dp, -9.72_dp]
+      A(2, :) = [-3.4_dp, -1.9_dp, -8.67_dp]
+      A(3, :) = [-3.8_dp, -1.7_dp, 0.0_dp]
+      A(4, :) = [1.6_dp, -4.0_dp, 0.0_dp]
+      A(5, :) = [1.6_dp, -4.0_dp, 0.0_dp]
+      b = [1.02_dp, 0.03_dp, 0.081_dp]
+
+      ! Setup the constraints.
+      neq = 3; ncons = 3
+      !> Allocate data.
+      allocate (iact(ncons))
+      allocate (x, source=q); x = 0.0_dp
+      allocate (y(ncons)); y = 0.0_dp
+      !> Allocate workspace
+      r = min(n, ncons); lwork = 2*n + r*(r + 5)/2 + 2*ncons + 1
+      allocate (work(lwork)); work = 0.0_dp
+      !> Solve the QP problem.
+      info = 0 ! P is already factorized when defining the QP.
+      call qpgen2(P, q, n, n, x, y, obj, A, b, n, ncons, neq, iact, nact, iter, work, info)
+
+      !> Check QuadProg info message.
+      call check(error, info == 0, &
+                 "QP solver did not converge.")
+      if (allocated(error)) return
+
+      !> Check solution correctness.
+      xref = [0.07313507_dp, -0.09133482_dp, -0.08677699_dp, 0.03638213_dp, 0.03638213_dp]
+      yref = [0.0440876_dp, 0.01961271_dp, 0.08465554_dp]
+      obj_ref = 0.0393038880729888_dp
+
+      !> Constrained solution.
+      call check(error, norm2(xref - x) < 1e-6_dp, "Constrained solution is not correct.")
+      if (allocated(error)) return
+      !> Lagrange multipliers.
+      call check(error, norm2(yref - y) < 1e-6_dp, "Lagrange mutilpliers are not correct.")
+      if (allocated(error)) return
+      !> Objective value.
+      call check(error, abs(obj_ref - obj) < 1e-6_dp, "Minimum cost is not correct.")
+      if (allocated(error)) return
+      return
+   end subroutine
+
    subroutine test_problem_3(error)
       type(error_type), allocatable, intent(out) :: error
       integer, parameter :: n = 5, m = 3
